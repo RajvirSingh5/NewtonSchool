7ae06f5d1996ed7c7f553a724f93d544383ba813
RajvirSingh5 <rajvir.singh5@hotmail.com>
2021-10-05 19:06:11 +0530

    5th oct

---
+++ React JS/WelcomeAssign_InClass.js
@@ -0,0 +1,22 @@
+import React, { Component} from "react";
+
+class ClassDemo extends Component {
+	
+	render() {
+		<h1>h1</hi>
+	}
+
+}
+
+//Same thing in functional component
+
+/*
+
+const ClassDemo = () + {
+	
+	return(
+		<h1>h1</hi>
+	)
+}
+
+*/
